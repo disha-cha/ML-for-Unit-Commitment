717d021d425293972f45acec703466bb12e8eac5
disha-cha <disha31@mit.edu>
2024-10-31 22:28:02 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-import sys
-
 """Example code to generate and solve UC MILP models"""
 
+import sys
 import pyomo.environ as pyo
 from helpers import parsecase, add_gens_to_case
 import pickle
@@ -13,21 +12,21 @@ import matplotlib.pyplot as plt
 from pyomo.opt import SolverStatus, TerminationCondition
 import json
 
+# Read command-line arguments
+num_solar = int(sys.argv[1])
+num_wind = int(sys.argv[2])
+num_hydro = int(sys.argv[3])
+num_batt = int(sys.argv[4])
+num_therm = int(sys.argv[5])
+
+# Load the network
 net = pandapower.networks.case_ieee30()
 original_therm = len(net.gen)
 
-num_solar = 3
-num_wind =3
-num_hydro =3
-num_batt = 3
-num_therm = 3
-
-
-
-# Usage
+# Add generators to the network
 net = add_gens_to_case(net, num_solar, num_wind, num_batt, num_hydro, num_therm)
 
-# single helper function so that the kwargs are the same
+# Define kwargs for parsing and model generation
 kwargs = {
     'num_solar': num_solar,
     'num_wind': num_wind,
@@ -42,85 +41,79 @@ kwargs = {
     'num_demands': len(net.load)
 }
 
+# Create model name
 model_name = "UCModel" + "_".join([key + "=" + str(kwargs[key]) for key in kwargs])
 
+# Parse case and get data
 db = parsecase(net, **kwargs)
 
+# Load data
 with open("UCdata.p", "rb") as f:
     p_data = pickle.loads(pickle.load(f))
 
-
-from main import opt_model_generator
-
+# Update kwargs for model generation
 opt_model_kwargs = kwargs.copy()
-
 num_existing_therm = original_therm
 opt_model_kwargs['num_existing_therm'] = num_existing_therm
 
+# Get slack bus ID
 slack_bus = p_data[None].get("slack_bus")
+opt_model_kwargs['slack_bus_id'] = slack_bus
 
+# Generate the optimization model
+model = ucml.opt_model_generator(**opt_model_kwargs)
 
-opt_model_kwargs = {
-    'num_solar': num_solar,
-    'num_wind': num_wind,
-    'num_hydro': num_hydro,
-    'num_batt': num_batt,
-    'num_existing_therm': num_existing_therm,
-    'num_therm': num_therm,
-    'num_nodes': len(net.bus),
-    'num_lines': len(net.line),
-    'time_periods': 24,
-    'num_scenarios': 1,
-    'num_uncert': 1,
-    'num_demands': len(net.load),
-    'slack_bus_id': slack_bus
-}
-
-model = opt_model_generator(**opt_model_kwargs)
-# model.pprint()
-
+# Create an instance of the model
 instance = model.create_instance(data=p_data)
-# make into MPS file
-# instance.write("data/" + model_name + ".mps")
 
+# Save the model as MPS file
+instance.write("data/" + model_name + ".mps")
+
+# Solve the model
 solver = pyo.SolverFactory('gurobi')
 result = solver.solve(instance, tee=False)
 
+# Check solver status
 if result.solver.status == SolverStatus.ok and result.solver.termination_condition == TerminationCondition.optimal:
     if hasattr(result.problem, 'upper_bound') and hasattr(result.problem, 'lower_bound'):
         primal_bound = result.problem.upper_bound
         dual_bound = result.problem.lower_bound
-        print("primal bound:", primal_bound)
-        print("dual bound:", dual_bound)
-    # with open("data/" + model_name + ".json", "w") as out:
-    #     out.write(json.dumps({"dual_bound": dual_bound, "primal_bound": primal_bound}))
-    print("optimal value:", pyo.value(instance.obj))
+        print("Primal bound:", primal_bound)
+        print("Dual bound:", dual_bound)
+    else:
+        primal_bound = pyo.value(instance.obj)
+        dual_bound = None
+    # Save results to JSON file
+    with open("data/" + model_name + ".json", "w") as out:
+        out.write(json.dumps({"dual_bound": dual_bound, "primal_bound": primal_bound}))
+    print("Optimal value:", pyo.value(instance.obj))
 else:
-    print("NO OPTIMAL VALUE FOUND")
+    print("No optimal value found")
 
-# make a file with output json file, with name
-# Extract and Plot Results
+# Extract and plot results
 num_renew = num_solar + num_wind
-num_gen = num_renew + num_hydro + num_batt + num_therm
+num_gen = original_therm + num_therm + num_hydro + num_solar + num_wind + num_batt
 
 # Define generator groups based on unique IDs
-Gtherm = range(1, num_therm + 1)
-Ghydro = range(num_therm + 1, num_therm + num_hydro + 1)
-Gsolar = range(num_therm + num_hydro + 1, num_therm + num_hydro + num_solar + 1)
-Gwind = range(num_therm + num_hydro + num_solar + 1, num_therm + num_hydro + num_solar + num_wind + 1)
-Gbatt = range(num_therm + num_hydro + num_solar + num_wind + 1, num_gen + 1)
+Gtherm = range(1, original_therm + num_therm + 1)
+Ghydro = range(Gtherm.stop, Gtherm.stop + num_hydro)
+Gsolar = range(Ghydro.stop, Ghydro.stop + num_solar)
+Gwind = range(Gsolar.stop, Gsolar.stop + num_wind)
+Gbatt = range(Gwind.stop, Gwind.stop + num_batt)
 Grenew = list(Gsolar) + list(Gwind)
 G = list(Gtherm) + list(Ghydro) + list(Gsolar) + list(Gwind) + list(Gbatt)
 
-
+# Create a DataFrame for plotting
 df = pd.DataFrame({
-    'thermal': [sum(pyo.value(instance.p[g, t, s]) for g in Gtherm for s in range(1, kwargs['num_scenarios'] + 1)) for t in range(1, kwargs['time_periods'] + 1)],
-    'solar': [sum(pyo.value(instance.p[g, t, s]) for g in Gsolar for s in range(1, kwargs['num_scenarios'] + 1)) for t in range(1, kwargs['time_periods'] + 1)],
-    'wind': [sum(pyo.value(instance.p[g, t, s]) for g in Gwind for s in range(1, kwargs['num_scenarios'] + 1)) for t in range(1, kwargs['time_periods'] + 1)],
-    'hydro': [sum(pyo.value(instance.p[g, t, s]) for g in Ghydro for s in range(1, kwargs['num_scenarios'] + 1)) for t in range(1, kwargs['time_periods'] + 1)],
-    'battery': [sum(pyo.value(instance.p[g, t, s]) for g in Gbatt for s in range(1, kwargs['num_scenarios'] + 1)) for t in range(1, kwargs['time_periods'] + 1)],
+    'thermal': [sum(pyo.value(instance.p[g, t, 1]) for g in Gtherm) for t in range(1, kwargs['time_periods'] + 1)],
+    'solar': [sum(pyo.value(instance.p[g, t, 1]) for g in Gsolar) for t in range(1, kwargs['time_periods'] + 1)],
+    'wind': [sum(pyo.value(instance.p[g, t, 1]) for g in Gwind) for t in range(1, kwargs['time_periods'] + 1)],
+    'hydro': [sum(pyo.value(instance.p[g, t, 1]) for g in Ghydro) for t in range(1, kwargs['time_periods'] + 1)],
+    'battery': [sum(pyo.value(instance.pdchg[g, t, 1]) - pyo.value(instance.pchg[g, t, 1]) for g in Gbatt)
+                for t in range(1, kwargs['time_periods'] + 1)],
 })
 
+# Plot the results
 ax = df.plot.area(stacked=True)
 plt.xlabel('Time Period')
 plt.ylabel('Power Output (MW)')
